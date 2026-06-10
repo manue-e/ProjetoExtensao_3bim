c61f4de4d4205338b4f6f931af146bf8c13eefd8
matheuscostar77 <matheuscostarib@gmail.com>
2026-06-09 21:39:39 -0300

    atualização no arquivo do firebase

--- js/firebase.js
+++ js/firebase.js
@@ -1,18 +1,21 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
-// Your web app's Firebase configuration
+import { getAuth }
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCTTALaxakoIoSB0JZQ2h7Q-nxq837Zy34",
-  authDomain: "daedalo-bd.firebaseapp.com",
-  databaseURL: "https://daedalo-bd-default-rtdb.firebaseio.com",
-  projectId: "daedalo-bd",
-  storageBucket: "daedalo-bd.firebasestorage.app",
-  messagingSenderId: "577862443103",
-  appId: "1:577862443103:web:2313536f5bd82d2e9bbbea"
+    apiKey: "AIzaSyCTTALaxakoIoSB0JZQ2h7Q-nxq837Zy34",
+    authDomain: "daedalo-bd.firebaseapp.com",
+    databaseURL: "https://daedalo-bd-default-rtdb.firebaseio.com",
+    projectId: "daedalo-bd",
+    storageBucket: "daedalo-bd.firebasestorage.app",
+    messagingSenderId: "577862443103",
+    appId: "1:577862443103:web:2313536f5bd82d2e9bbbea"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export { auth };
